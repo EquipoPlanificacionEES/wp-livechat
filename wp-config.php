669150ec077450c35b88185e149f878feba00253
enidev911 <enidev911@gmail.com>
2025-03-08 21:23:59 -0300

    up: environment variables

--- wp-config.php
+++ wp-config.php
@@ -20,22 +20,28 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wordpress' );
+// define( 'DB_NAME', 'wordpress' );
 
-/** Database username */
-define( 'DB_USER', 'root' );
+// /** Database username */
+// define( 'DB_USER', 'root' );
 
-/** Database password */
-define( 'DB_PASSWORD', 'root' );
+// /** Database password */
+// define( 'DB_PASSWORD', 'root' );
 
-/** Database hostname */
-define( 'DB_HOST', 'localhost' );
+// /** Database hostname */
+// define( 'DB_HOST', 'localhost' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
 
 /** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
+
+define('DB_NAME', getenv("MYSQL_ADDON_DB"));
+define('DB_USER', getenv("MYSQL_ADDON_USER"));
+define('DB_PASSWORD', getenv("MYSQL_ADDON_PASSWORD"));
+define('DB_HOST', getenv("MYSQL_ADDON_HOST") . ":" . getenv("MYSQL_ADDON_PORT"));
+
 
 /**#@+
  * Authentication unique keys and salts.
